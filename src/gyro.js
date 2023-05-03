ff15e1a61410ef4f8477aef3674ead44b7ebcbee
Allan <allan.duche@gmail.com>
2023-05-03 14:44:43 +0200

    alert

--- src/gyro.js
+++ src/gyro.js
@@ -2,6 +2,7 @@ let gyroscope = new Gyroscope({ frequency: 60 });
 
 gyroscope.addEventListener("reading", (e) => {
   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  alert(gyroscope.x);
   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
 });
