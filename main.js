0cd297d60750c2283a2640bb7f67bb6fee4be6b1
Allan <allan.duche@gmail.com>
2023-05-03 14:34:59 +0200

    test gyro values

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import gyro from "./gyro.js"
 
 document.querySelector('#app').innerHTML = `
   <div>
-
+    ${gyro.x}
+    <br>
+    ${gyro.y}
+    <br>
+    ${gyro.z}
   </div>
 `
+
 setupCounter(document.querySelector('#counter'))
---
+++ src/gyro.js
@@ -0,0 +1,13 @@
+let gyroscope = new Gyroscope({ frequency: 60 });
+
+gyroscope.addEventListener("reading", (e) => {
+  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+});
+gyroscope.start();
+
+
+export default {x : gyroscope.x, y: gyroscope.y, z: gyroscope.z}
+
+
