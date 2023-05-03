686a3a5f74cb226720238063fc05cdece8a911e3
Allan <allan.duche@gmail.com>
2023-05-03 15:09:36 +0200

    undo

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 import './style.css'
-import gyroscope from './src/gyro'
-
-document.querySelector('#app').innerHTML = 
-`
-<div>
-  ${gyroscope.x ? gyroscope.x : 0}
-  <br>
-  ${gyroscope.y ? gyroscope.y : 0}
-  <br>
-  ${gyroscope.z ? gyroscope.z : 0}
-</div>
-`
+import './src/gyro'
--- src/gyro.js
+++ src/gyro.js
@@ -1,22 +1,25 @@
-let gyroscope = {x : 0, y: 0, z: 0}
+let gyroscope = new Gyroscope({ frequency: 60 });
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function (event) {
-       // tilt([event.beta, event.gamma]);
-    }, true);
-} else if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', function (event) {
-      //  tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
-    }, true);
-} else {
-    window.addEventListener("MozOrientation", function (orientation) {
-        gyroscope.x = orientation.x
-        gyroscope.y = orientation.y
-        gyroscope.y = orientation.z
-    }, true);
-}
+gyroscope.addEventListener("reading", (e) => {
+  alert(gyroscope.x);
+  displayHtml();
+});
 
+gyroscope.addEventListener("error", (e) => {
+  alert('Gyro not working');
+});
 
-export default gyroscope
+gyroscope.start();
 
-
+const displayHtml = () => {
+    setInterval(function () {  
+      document.querySelector('#app').innerHTML = `
+      <div>
+        ${gyroscope.x ? gyroscope.x : 0}
+        <br>
+        ${gyroscope.y ? gyroscope.y : 0}
+        <br>
+        ${gyroscope.z ? gyroscope.z : 0}
+      </div>
+    `}, 100);
+};
