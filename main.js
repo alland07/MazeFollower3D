dfd3f3c3fbe5f4582a3165ade74847256badeca2
Allan <allan.duche@gmail.com>
2023-05-03 14:37:51 +0200

    test gyro values

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import gyro from "./gyro.js"
+import gyro from "./src/gyro.js"
+
 
 document.querySelector('#app').innerHTML = `
   <div>
-    ${gyro.x}
+    ${gyro.x ? gyro.x : 0}
     <br>
-    ${gyro.y}
+    ${gyro.y ? gyro.y : 0}
     <br>
-    ${gyro.z}
+    ${gyro.z ? gyro.z : 0}
   </div>
 `
 
