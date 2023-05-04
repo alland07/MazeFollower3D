2514d082d5d78c56a2062bb8800bdad5dd5e8555
Allan <allan.duche@gmail.com>
2023-05-04 16:03:22 +0200

    test

--- src/accel.js
+++ src/accel.js
@@ -5,22 +5,20 @@ const dataPoint = new DataPoint();
 
 accel.addEventListener("reading", (e) => {
   dataPoint.pushValue(accel.x, accel.y, accel.z);
-  setInterval(() => {
-   alert(accel.x);
-  }, 10000)
   //dataPoint.derivate();
+
   getAccel();
 }); 
 
 accel.start();
 
 const getAccel = () => {
-  const {x,y,z} = dataPoint.getDataFilter();
+  // const {x,y,z} = dataPoint.getDataFilter();
 
-  const absoluteX = Math.abs(x);
-  const absoluteY = Math.abs(y);
-  const absoluteZ = Math.abs(z);
-  if (absoluteX > 2 || absoluteY > 2 || absoluteZ > 2) {
+  const absoluteX = Math.abs(accel.x);
+  const absoluteY = Math.abs(accel.y);
+  const absoluteZ = Math.abs(accel.z);
+  if (absoluteX > 20 || absoluteY > 20 || absoluteZ > 20) {
     changeMesh(absoluteX,absoluteY,absoluteZ);
   }
   
